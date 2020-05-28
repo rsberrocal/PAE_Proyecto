da50d414f54b8df7cc89c9fda9bc21aeb99faaf1
Richard <rsberrocal@gmail.com>
2020-05-29 00:49:29 +0200

    Creado cmake

---
+++ cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/dyn/dyn_app_common.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/dyn/dyn_app_common.c.o"
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/dyn/dyn_app_sensor.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/dyn/dyn_app_sensor.c.o"
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/dyn/dyn_frames.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/dyn/dyn_frames.c.o"
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/dyn/dyn_instr.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/dyn/dyn_instr.c.o"
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/dyn/dyn_motors.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/dyn/dyn_motors.c.o"
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/dyn_test/b_queue.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/dyn_test/b_queue.c.o"
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/dyn_test/dyn_emu.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/dyn_test/dyn_emu.c.o"
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/dyn_test/movement_simulator.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/dyn_test/movement_simulator.c.o"
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/hal_dyn_uart/hal_dyn_uart_emu.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/hal_dyn_uart/hal_dyn_uart_emu.c.o"
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/joystick_emu/joystick.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/joystick_emu/joystick.c.o"
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/main.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/main.c.o"
+  "/cygdrive/e/Universidad/PAE/PAE_Proyecto/posicion.c" "/cygdrive/e/Universidad/PAE/PAE_Proyecto/cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/posicion.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../dyn"
+  "../dyn_test"
+  "../hal_dyn_uart"
+  "../joystick_emu"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PAE_Proyecto.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PAE_Proyecto.dir/dyn/dyn_app_common.c.o"
+  "CMakeFiles/PAE_Proyecto.dir/dyn/dyn_app_sensor.c.o"
+  "CMakeFiles/PAE_Proyecto.dir/dyn/dyn_frames.c.o"
+  "CMakeFiles/PAE_Proyecto.dir/dyn/dyn_instr.c.o"
+  "CMakeFiles/PAE_Proyecto.dir/dyn/dyn_motors.c.o"
+  "CMakeFiles/PAE_Proyecto.dir/dyn_test/b_queue.c.o"
+  "CMakeFiles/PAE_Proyecto.dir/dyn_test/dyn_emu.c.o"
+  "CMakeFiles/PAE_Proyecto.dir/dyn_test/movement_simulator.c.o"
+  "CMakeFiles/PAE_Proyecto.dir/hal_dyn_uart/hal_dyn_uart_emu.c.o"
+  "CMakeFiles/PAE_Proyecto.dir/joystick_emu/joystick.c.o"
+  "CMakeFiles/PAE_Proyecto.dir/main.c.o"
+  "CMakeFiles/PAE_Proyecto.dir/posicion.c.o"
+  "PAE_Proyecto.exe"
+  "PAE_Proyecto.pdb"
+  "libPAE_Proyecto.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PAE_Proyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
